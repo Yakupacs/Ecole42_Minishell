aed30997014783a0f28dbf01f12a0d78c4780dc7
yakupacs <ykpacs@gmail.com>
2023-07-22 11:12:23 +0300

    Fixed.

--- minishell/ft_main.c
+++ minishell/ft_main.c
@@ -6,11 +6,53 @@
 /*   By: yacis <yacis@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 00:04:55 by yacis             #+#    #+#             */
-/*   Updated: 2023/07/20 19:24:08 by yacis            ###   ########.fr       */
+/*   Updated: 2023/07/22 11:09:15 by yacis            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	**copy_env(char **envp)
+{
+	int		i;
+	char	**copy_env;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	copy_env = malloc(sizeof(char *) * (i + 1));
+	i = 0;
+	while (envp[i])
+	{
+		copy_env[i] = ft_strdup(envp[i]);
+		i++;
+	}
+	copy_env[i] = NULL;
+	return (copy_env);
+}
+
+void	ft_init_global(char **envp, int flag)
+{
+	if (flag)
+	{
+		g_global.ex_path = copy_env(envp);
+		g_global.envp = copy_env(envp);
+	}
+	g_global.signal_status = 0;
+	g_global.list = NULL;
+	g_global.command = NULL;
+	g_global.redirection = NULL;
+	g_global.count_type = NULL;
+	g_global.heredoc = NULL;
+	g_global.line = NULL;
+	g_global.sig_flag = 0;
+	g_global.error_flag = 0;
+	g_global.here_fd = 0;
+	g_global.heredoc_flag = 0;
+	g_global.dollar_flag = 0;
+	g_global.sighandler_flag = 0;
+	g_global.execute_signal_flag = 0;
+}
 
 int	continue_loop(t_command *cpy_g_global_list)
 {
